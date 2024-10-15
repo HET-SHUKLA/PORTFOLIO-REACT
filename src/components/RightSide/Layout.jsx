d5d92fe859b05878a89438093eb0acf8127b5308
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-15 22:51:23 +0530

    Add Outlet in Layout.jsx

--- src/components/RightSide/Layout.jsx
+++ src/components/RightSide/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Outlet} from 'react-router-dom';
+import Menu from '../LeftSide/Menu';
 
 const Layout = () => {
     return (
         <>
-            
+            <Menu />
+            <Outlet />
         </>
     );
 }
