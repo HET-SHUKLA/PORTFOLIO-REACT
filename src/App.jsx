cf9351114ef25e1ce69961e6fab3fb87be35cc24
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-15 23:00:09 +0530

    add Route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import Layout from './components/RightSide/Layout';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // Root Layout with Outlet
+    <Route path='/' element={<Layout />}>
+
+
+    </Route>
+  )
+)
 
 const App = () => {
   return (
