dccaed8cd6c0d188c4a7217d6795644bf62d5e24
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-15 22:53:41 +0530

    Add About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <>
+          Welcome to the About.jsx
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ullam sit delectus quam eveniet eligendi nemo magni! Esse earum expedita, ex labore autem molestias at eius impedit, aperiam accusantium a optio explicabo cum corporis architecto molestiae, temporibus similique voluptatem doloremque? Perspiciatis porro ratione officiis voluptas cumque accusamus et facere quos.  
+        </>
+    );
+}
+
+export default About;
