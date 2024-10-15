3c9e73a7ed69d8684691ef91330809c39d312502
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-16 00:23:43 +0530

    Created animation.css

--- src/components/LeftSide/Menu.jsx
+++ src/components/LeftSide/Menu.jsx
@@ -6,14 +6,18 @@ import aboutImage from '/about.png';
 const Menu = () => {
     return (
         <div className='h-screen flex items-center mx-10'>
+
             <div className='flex flex-col justify-evenly items-center h-3/4 rounded-md p-2 border border-purple-500'>
+            
                 <div className='w-8 h-8'>
                     <Link to=""> <img src={profileImage} alt="Profile" /> </Link>
                 </div>
+
                 <div className='w-8 h-8'>
                     <Link to="/about"> <img src={aboutImage} alt="About" /> </Link>
                 </div>
             </div>
+
         </div>
     );
 }
