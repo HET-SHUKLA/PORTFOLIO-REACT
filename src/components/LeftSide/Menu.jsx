2cd1212d377969d7b5405684f42605fe532c96e0
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-16 01:00:50 +0530

    Implement dynamic strech on menu

--- src/components/LeftSide/Menu.jsx
+++ src/components/LeftSide/Menu.jsx
@@ -3,15 +3,27 @@ import profileImage from '/profile.png';
 import aboutImage from '/about.png';
 import '../../animations.css';
 import MenuItem from './MenuItem';
+import { useState } from 'react';
 
 const Menu = () => {
-    return (
-        <div className='animation-strech h-screen flex items-center mx-10'>
 
-            <div className='flex flex-col justify-evenly items-center h-3/4 rounded-md p-2 border border-purple-500'>
+    const [hovers, setHovers] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHovers(true);
+    }
+
+    const handleMouseLeave = () => {
+        setHovers(false);
+    }
+
+    return (
+        <div className='h-screen flex items-center mx-10'>
+
+            <div className='flex flex-col justify-evenly items-center h-3/4 rounded-md p-2 border border-purple-500' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             
-                <MenuItem image={profileImage} text={"Profile"} />
-                <MenuItem image={aboutImage} text={"About"} to={"about"} />
+                <MenuItem image={profileImage} text={"Profile"} hovers={hovers} />
+                <MenuItem image={aboutImage} text={"About"} to={"about"} hovers={hovers} />
 
             </div>
 
--- src/components/LeftSide/MenuItem.jsx
+++ src/components/LeftSide/MenuItem.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const MenuItem = ({image, text, to=""}) => {
+const MenuItem = ({image, text, to="", hovers=false}) => {
+
     return (
         <>
             <div className=''>
                 <Link to={to}>
-                    <div className='flex'>
+                    <div className='flex items-center'>
                         <div className='w-8 h-8'>
                             <img src={image} alt={text} /> 
                         </div>
-                        <span className='hidden'>{text}</span>
+                        <div className={`ml-3 ${!hovers && "hidden"}`}>
+                            <p>{text}</p>
+                        </div>
                     </div>
                 </Link>
             </div>
