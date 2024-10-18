acb64a24f83d0baca89d21205fa5791a99c404ae
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-19 01:18:22 +0530

    Update TechStack.jsx

--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const TechStack = () => {
     return (
-        <div>
-            Hrllo
-        </div>
+        <>
+            <div>
+                <div>
+                    <h2 className='text-5xl'>Tech Stack</h2>
+                </div>
+
+                <div>
+                    <strong>Frontend : </strong> React js, XML, HTML, CSS, JS
+                </div>
+
+                <div>
+                    <strong>Backend : </strong> Node js, Express js, ASP.NET, PHP
+                </div>
+
+                <div>
+                    <strong>Database : </strong> MongoDB, Postgres, Oracle
+                </div>
+            </div>
+        </>
     );
 }
 
