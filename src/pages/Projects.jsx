a70a737ac37adcd13d02ef904af32423bae9acd9
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-21 01:01:54 +0530

    Trying o solve a bug

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,28 @@
+import "react-multi-carousel/lib/styles.css";
 import React, { Fragment } from 'react';
 import ProjectCard from '../components/ProjectCard';
 import '../index.css';
 import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 const Projects = ({textColor='white', wh='w-11/12'}) => {
     return (
@@ -12,31 +32,12 @@ const Projects = ({textColor='white', wh='w-11/12'}) => {
                 <h2 className={`text-${textColor} text-5xl`}>Projects</h2>
             </div>
 
-            <Carousel 
-                swipeable={true}
-                draggable={false}
-                showDots={true}
-                infinite={true}
-                autoPlaySpeed={1000}
-                keyBoardControl={true}
-                customTransition="all .5"
-                transitionDuration={500}
-                containerClass="carousel-container"
-                removeArrowOnDeviceType={["tablet", "mobile"]}
-                dotListClass="custom-dot-list-style"
-                itemClass="carousel-item-padding-40-px"
-            >
-                {/* <div className='flex flex-wrap h-full w-full overflow-scroll hide-scroll'>
-                    <ProjectCard projectDesc={'Project Description     Will Come     HERe \n Ok?'} projectTech={'REACT, TAILWIND'} projectImg={'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} projectTitle={'DEMO PROJECT'} projectLink={'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3'} />
-                    
-
-                    
-                </div> */}
-                <div>Item 1</div>
-  <div>Item 2</div>
-  <div>Item 3</div>
-  <div>Item 4</div>
-            </Carousel>
+            <Carousel responsive={responsive} containerClass="carousel-container" itemClass="carousel-item-padding-40-px">
+    <div style={{ height: '200px', backgroundColor: 'lightgray' }}>Item 1</div>
+    <div style={{ height: '200px', backgroundColor: 'lightgray' }}>Item 2</div>
+    <div style={{ height: '200px', backgroundColor: 'lightgray' }}>Item 3</div>
+    <div style={{ height: '200px', backgroundColor: 'lightgray' }}>Item 4</div>
+</Carousel>
         </div>
     );
 }
