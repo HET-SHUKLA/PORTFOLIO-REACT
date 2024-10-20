4087e8df90317bcdf5244e3984cbee4f3b4e4bc3
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-21 00:02:30 +0530

    Done TechStack

---
+++ src/components/TechSkill.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TechSkill = ({icon, title}) => {
+    console.log(icon);
+    
+    return (
+        <>
+            <div className='bg-gray-900 flex py-2 px-4 mx-2 mt-6 rounded-lg items-center hover:scale-110 cursor-pointer'>
+                <div>
+                    <img src={icon} alt="Icon" className='w-10' />
+                </div>
+
+                <div className='mx-5 text-center'>
+                    <h4 className={`text-white text-md`}>{title}</h4>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default TechSkill;
