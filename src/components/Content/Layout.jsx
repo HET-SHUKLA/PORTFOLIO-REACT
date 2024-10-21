1cbdd60e70d76aeac3da95cc81451150ddaeb274
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-21 19:28:58 +0530

    changeee

--- src/components/Content/Layout.jsx
+++ src/components/Content/Layout.jsx
@@ -7,15 +7,9 @@ import Footer from '../Bottom/Footer';
 
 const Layout = () => {
 
-    let sc = 0;
-
     const wh = 'w-11/12';
 
     const [isVisible, setIsVisible] = useState(false);
-    const [isAbout, setIsAbout] = useState(false);
-    const [isTech, setIsTech] = useState(false);
-    const [isProject, setIsProject] = useState(false);
-    const [isContact, setIsContact] = useState(false);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -25,26 +19,10 @@ const Layout = () => {
         return () => clearTimeout(timer); // Clean up the timer
     }, []);
 
-    const handleScroll = (e) => {
-        sc++;
-
-        if(sc > 5){
-            setIsAbout(true);
-        }
-        if(sc > 25){
-            setIsProject(true);
-        }
-        if(sc > 15){
-            setIsTech(true);
-        }
-        if(sc > 35){
-            setIsContact(true);
-        }
-    }
 
     return (
         <>
-            <div className={`flex flex-col h-screen w-screen overflow-auto hide-scroll app ${isVisible ? 'visible' : ''}`}  onScroll={handleScroll}>
+            <div className={`flex flex-col h-screen w-screen overflow-auto hide-scroll app ${isVisible ? 'visible' : ''}`} >
                 <div className='flex flex-col w-full'>
                     <div className={`w-full flex justify-center fixed z-10 `}>
                         <Menu />
@@ -54,7 +32,7 @@ const Layout = () => {
                             <Home textColor={'white'} />
                         </section>
 
-                        <section className={`${wh} control my-10 p-5 flex flex-col items-center `} id='about'>
+                        <section className={`${wh} control my-10 p-5 flex flex-col items-center`} id='about'>
                             <About textColor={'white'} />
                         </section>
 
