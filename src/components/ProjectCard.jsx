0d3b5260476cfe157874ae9f2d107b160e3ce7b3
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-21 14:20:38 +0530

    Done Project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import '../index.css';
 
-const ProjectCard = ({projectImg, projectTitle, projectDesc, projectTech, projectLink=null}) => {
+const ProjectCard = ({projectImg, projectTitle, projectDesc, projectTech, live, github}) => {
     return (
         <>
         
@@ -10,18 +11,32 @@ const ProjectCard = ({projectImg, projectTitle, projectDesc, projectTech, projec
                     <img src={projectImg} alt='Project Image' className='h-40 rounded-lg' />
                 </div>
 
-                <div>
-                    <h3 className='text-white'><a href={projectLink}>{projectTitle}</a></h3>
+                <div className='mt-2'>
+                    <h3 className='text-white text-center text-2xl'>{projectTitle}</h3>
                 </div>
 
-                <div>
-                    <h3 className='text-white'>{projectTech}</h3>
+                <div className='flex mt-2'>
+                    <h3 className='text-white'>
+                    <strong className='mr-2'>Tech stack : </strong>
+                        {projectTech}
+                        </h3>
                 </div>
 
-                <div>
+                <div className='flex mt-2 h-52 overflow-scroll hide-scroll'>
                     <p className='text-white'>
+                    <strong className='mr-2'>Description : </strong>
                         {projectDesc}
                     </p>
+                </div>
+
+                <div className="grid grid-cols-2 mt-5 ">
+                    <a href={live} target='_blank' className='bg-gray-950 hover:bg-black p-2 rounded-lg text-white text-center cursor-pointer'>
+                        Link
+                    </a>
+
+                    <a href={github} target='_blank' className='bg-gray-950 hover:bg-black p-2 rounded-lg text-white ms-3 text-center cursor-pointer'>
+                        Github
+                    </a>
                 </div>
 
             </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ProjectCard from '../components/ProjectCard';
 import '../index.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -7,6 +7,11 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import news from '/projects/news.png';
+import whisper from '/projects/whisper.png';
+import portfolio from '/projects/portfolio.png';
+import blog from '/projects/blog.png';
+
 
 const Projects = ({textColor='white', wh='w-11/12'}) => {
     return (
@@ -22,7 +27,6 @@ const Projects = ({textColor='white', wh='w-11/12'}) => {
                 slidesPerView={3} // Number of items per view
                 navigation={true} // Enables arrows for navigation
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
-                pagination={{ clickable: true }}
                 scrollbar={{ draggable: true }}
                 breakpoints={{
                 1024: { slidesPerView: 3 },
@@ -30,39 +34,51 @@ const Projects = ({textColor='white', wh='w-11/12'}) => {
                 640: { slidesPerView: 1 },
                 }}
             >
-                <SwiperSlide className='h-full'>
-                    <div className='h-full'>
-                        <ProjectCard projectImg={'https://images.unsplash.com/photo-1719937051058-63705ed35502?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} projectDesc={'Bets Project'} projectTech={'REACT, REACT, REACT'} projectTitle={'REACT'}  />
+                <SwiperSlide className=''>
+                    <div className=''>
+                        <ProjectCard 
+                        projectImg={whisper} 
+                        projectTitle={'Whisper'} 
+                        projectTech={'Kolin, XML, Firebase, FCM'}
+                        projectDesc={'Whisper is a E2EE(Using Deffie Hellman Algo) private social media application with video call, voice call and Reat time chat functionality. Other than that it also include functionalities like Follow, Unfollow, Post, Upvote, Downvote, Dynamic app theme, Location sharing, Contact Sharing, Gemini AI, can have Multiple accounts from single email, search user, Give Alias to the follower for notification.'}   
+                        live={'https://drive.google.com/file/d/1cJdGB4zTX88li6ND9aCXjhRY4BHeRHf2/view'} 
+                        github={'https://github.com/HET-SHUKLA/Whisper'}  />
                     </div>
                 </SwiperSlide>
 
-                <SwiperSlide>
-                    <div>
-                        <ProjectCard projectImg={'https://images.unsplash.com/photo-1719937051058-63705ed35502?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} projectDesc={'Bets Project'} projectTech={'REACT, REACT, REACT'} projectTitle={'REACT'}  />
+                <SwiperSlide className=''>
+                    <div className=''>
+                        <ProjectCard 
+                        projectImg={news} 
+                        projectTitle={'News Teller'} 
+                        projectTech={'Node JS, Express JS, EJS, REST API, HTML, CSS'}
+                        projectDesc={'News Teller is a web app that allows user to see news from all over the world. User can filter the news based on category as well as region. Also user can search perticular news. This web app is created using NEWS API and in free version it can only handle 50 requests per day.'}   
+                        live={'https://newsteller-9rfw.onrender.com/'} 
+                        github={'https://github.com/HET-SHUKLA/NewsTeller'}  />
                     </div>
                 </SwiperSlide>
 
-                <SwiperSlide>
-                    <div>
-                        <ProjectCard projectImg={'https://images.unsplash.com/photo-1719937051058-63705ed35502?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} projectDesc={'Bets Project'} projectTech={'REACT, REACT, REACT'} projectTitle={'AAA'}  />
+                <SwiperSlide className=''>
+                    <div className=''>
+                        <ProjectCard 
+                        projectImg={portfolio} 
+                        projectTitle={'Portfolio website'} 
+                        projectTech={'React, Tailwind'}
+                        projectDesc={'Personal portfolio website, I have created using REACT, this was my 2nd React projects.'}   
+                        live={'#'} 
+                        github={'https://github.com/HET-SHUKLA/PORTFOLIO-REACT'}  />
                     </div>
                 </SwiperSlide>
 
-                <SwiperSlide>
-                    <div>
-                        <ProjectCard projectImg={'https://images.unsplash.com/photo-1719937051058-63705ed35502?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} projectDesc={'Bets Project'} projectTech={'REACT, REACT, REACT'} projectTitle={'VVV'}  />
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <div>
-                        <ProjectCard projectImg={'https://images.unsplash.com/photo-1719937051058-63705ed35502?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} projectDesc={'Bets Project'} projectTech={'REACT, REACT, REACT'} projectTitle={'RRR'}  />
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <div>
-                        <ProjectCard projectImg={'https://images.unsplash.com/photo-1719937051058-63705ed35502?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} projectDesc={'Bets Project'} projectTech={'REACT, REACT, REACT'} projectTitle={'REACT'}  />
+                <SwiperSlide className=''>
+                    <div className=''>
+                        <ProjectCard 
+                        projectImg={blog} 
+                        projectTitle={'Blog App'} 
+                        projectTech={'React, Tailwind, TinyMCE, AppWrite'}
+                        projectDesc={'It is just a simple blog app, I have created by watching tutorial, Also this was my first ever React app.'}   
+                        live={'https://github.com/HET-SHUKLA/React-Blog-Webapp'} 
+                        github={'https://github.com/HET-SHUKLA/React-Blog-Webapp'}  />
                     </div>
                 </SwiperSlide>
             </Swiper>
