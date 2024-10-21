8b768c473d7fb4dba4678d4a7f57bc3244fe9fa2
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-21 17:46:37 +0530

    scrolled

--- src/components/Top/MenuItem.jsx
+++ src/components/Top/MenuItem.jsx
@@ -7,9 +7,14 @@ const MenuItem = ({image, text, to="", hovers=false}) => {
     return (
         <>
             <div className='hover:bg-black cursor-pointer p-3 rounded-lg'>
-                <a href={to} className='text-center'>
+                <button className='text-center' onClick={() => {
+                    const ele = document.querySelector(to);
+                    ele?.scrollIntoView({
+                        behavior: 'smooth'
+                    })
+                }} >
                     <p className='text-white hover:underline'>{text}</p>
-                </a>
+                </button>
             </div>
         </>
     );
